e76475ded0cde7e4e89367fe4724024acada93b6
Ambitiousdude <Augustine>
2023-02-16 18:34:08 -0800

    Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * Main - A c program that prints an exact word with put function
- * Return: 0
+ * Main - Entry point
+ * A c program that prints an exact word with put function
+ * Return: It gives 0
  */
 int main(void)
 {
