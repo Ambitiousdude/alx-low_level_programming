2381c23a8380665ef6f887d7ecb93f2af804b940
Ambitiousdude <Augustine>
2023-02-16 18:30:23 -0800

    Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Main - A c program that prints an exact word with put function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
